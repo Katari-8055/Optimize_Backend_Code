e1cb4809c485d316fd5ca973a613056769bc690a
Katari <kataricoder@gmail.com>
2025-12-13 02:25:40 +0530

    making small changes

--- backend/utils/asyncHandler.js
+++ backend/utils/asyncHandler.js
@@ -1,13 +1,15 @@
+//------------------------------------------------Using Promise-------------------------------------------------//
+
 // export const asyncHandler = (requestHandler) => {
 //     return async (req, res, next) => {
 //         Promise.resolve(requestHandler(req, res, next)).catch((err)=> next(err));
 //     }
 // }
 
-
+//------------------------------------------------Using try catch block-------------------------------------------------//
 // export const asyncHandler = (fn) => {}
 // export const asyncHandler = (fn) => {async () =>{} }
-
+//This is higher order function that takes a function as an argument and returns a new function
 export const asyncHandler = (fn) => async (req, res, next) =>{
     try {
         await fn(req, res, next);
