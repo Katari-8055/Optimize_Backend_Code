74feb5a0a1bf5ddeb7ab47c3f2191ccdfecddbad
Katari <kataricoder@gmail.com>
2025-12-15 18:41:38 +0530

    All controller setup configured

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import router from '../routes/user.route.js';
 
 dotenv.config();
 
@@ -18,5 +19,10 @@ app.use(express.json({limit: '16kb'})); // to parse JSON bodies
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static("public")); // to serve static files
 app.use(cookieParser()); // to parse cookies
+
+//Router declration 
+
+app.use("/api/v1/users", router);
+
 
 export default app;
